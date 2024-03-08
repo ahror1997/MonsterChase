e26ffa3c90226f91fb5f953c5f12b1fd2e3508b6
Akhror Ziyodinov <ahror.ziyodinov@gmail.com>
2024-03-08 20:42:31 +0100

    first commiy

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveForce = 10.0f;
+
+    [SerializeField]
+    public float jumpForce = 11.0f;
+    
+    private float movementX;
+    private Rigidbody2D myBody;
+    private SpriteRenderer sr;
+    private Animator animator;
+    private string WALK_ANIMATION = "Walk";
+
+    private bool isGrounded = true;
+    private string GROUND_TAG = "Ground";
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerJump();
+    }
+
+    private void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
+    }
+
+    private void AnimatePlayer()
+    {
+        if (movementX != 0)
+        {
+            animator.SetBool(WALK_ANIMATION, true);
+            sr.flipX = movementX > 0 ? false : true;
+        }
+        else
+        {
+            animator.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded = true;
+        }
+    }
+}
